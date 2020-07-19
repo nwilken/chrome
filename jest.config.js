e56dc963e3220a2cb2964f9ea8510ddcd73ba528
Nate Wilken <wilken@asu.edu>
2020-07-19 15:42:28 -0700

    add jest-html-reporter

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,10 @@ module.exports = {
         "**/tests/**/*.js"
     ],
     verbose: true,
+    reporters: [
+        "default",
+        ["./node_modules/jest-html-reporter", {
+            "pageTitle": "Test Report"
+        }]
+    ]
 }
